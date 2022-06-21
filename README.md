19ce63cafd0e63b1bd8686db642c186e5f5f2a0f
niccolocorsani <corsani.niccolo.94@gmail.com>
2022-06-21 15:07:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-8-area-chart-uzkvmh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-8-area-chart-uzkvmh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+// import * as chartData from './data.json';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  // If ctx does not work, get canvas via ViewChild()
+
+  dataArray: any = [];
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    let data: any,
+      options: any,
+      chart: any,
+      ctx: any = document.getElementById('areaChart') as HTMLElement;
+
+    // Stackblitz no longer supports local json files.
+    // Uncomment below and use import at top.
+    // Replace datasets with this.dataArray
+
+    // for (let key in chartData.items) {
+    //   if (chartData.items.hasOwnProperty(key)) {
+    //     this.dataArray.push(chartData.items[key]);
+    //   }
+    // }
+
+    data = {
+      labels: ['Apples', 'Oranges', 'Mixed Fruit'],
+      datasets: [
+        {
+          label: 'Apples',
+          data: [0, 50, 45, 100],
+          backgroundColor: 'rgba(40,125,200,.5)',
+          borderColor: 'rgb(40,100,200)',
+          fill: true,
+          lineTension: 0,
+          radius: 5,
+        },
+        {
+          label: 'Oranges',
+          data: [30, 90, 111, 20],
+          backgroundColor: 'rgba(75,10,125,.5)',
+          borderColor: 'rgb(75,10,125)',
+          fill: true,
+          lineTension: 0.2,
+          radius: 5,
+        },
+      ],
+    };
+
+    options = {
+      responsive: true,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        position: 'top',
+        text: 'Apples to Oranges',
+        fontSize: 12,
+        fontColor: '#666',
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+        labels: {
+          fontColor: '#999',
+          fontSize: 14,
+        },
+      },
+    };
+
+    chart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: options,
+    });
+  }
+}
